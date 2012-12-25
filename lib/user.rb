66410ec16177023944476100ced954bb5fff67b0
Akshay Gupta <akshay.aries11@gmail.com>
2012-12-25 23:58:21 +0530

    unit test cases for User class

--- lib/user.rb
+++ lib/user.rb
@@ -9,7 +9,11 @@ class User
   attr_accessor :name, :mobile, :role, :doj
   LOYALITY_YEARS = 2
     
-  # helps to create new user, assign roles  
+  # input: hash {:name, :mobile, :doj, :role}
+  # :name => "name"
+  # :mobile => "mobile number"
+  # :doj => Time object (optional), defaults: Time.now
+  # :role => Role (optional), defaults: Role::GENERAL  
   def initialize(options = {})
     raise Error::UserError, "Name can't be blank" unless options[:name]
     raise Error::UserError, "Mobile can't be blank" unless options[:mobile]
@@ -19,12 +23,14 @@ class User
     @role = options[:role].nil? ? Role::GENERAL : options[:role]
   end
   
-  # class method to generate a guest user
-  def self.guest_user(name = "Guest", mobile = "xxx")
-    User.new({:name => name, :mobile => mobile})
+  # class method to create a guest user on the fly
+  # input 'name', 'mobile', 'role'
+  def self.guest_user(name = "Guest", mobile = "xxx", role = Role::GENERAL)
+    User.new({:name => name, :mobile => mobile, :role => role})
   end
   
-  # check if a user is loyal or not
+  # returns true if a user is loyal and updates his role
+  # returns false if user is not loyal
   def loyal?
     if old_in_years > LOYALITY_YEARS 
       @role = Role::LOYAL if @role == Role::GENERAL || @role.nil?
@@ -34,7 +40,7 @@ class User
     end
   end
   
-  # calculates how old is a customer is at the retailer shop and hence used for loyality points
+  # calculates customer's joining date in number of years
   def old_in_years
     (Time.now - @doj)/(60*60*24*365) unless self.doj.nil?
   end
---
+++ test/unit/user_test.rb
@@ -0,0 +1,59 @@
+require File.dirname(__FILE__) + '/../../lib/user.rb'
+
+require "test/unit"
+
+
+# This is the test Class for person model
+# containing unit test cases for the same.
+
+class UserTest < Test::Unit::TestCase
+  
+  def test_minimum_years_required_to_be_loyal_user
+    assert_equal(User::LOYALITY_YEARS, 2)
+  end
+  
+  def test_user_name_is_required
+    assert_raise Error::UserError do 
+      User.new({:mobile => "9090909090"})
+    end
+  end
+  
+  def test_user_mobile_is_required
+    assert_raise Error::UserError do 
+      User.new({:name => "Kamal"})
+    end
+  end
+  
+  def test_zero_errors_when_both_name_and_mobile_are_provided
+    assert_nothing_raised Error::UserError do
+      user = User.new({:name => "Kamal", :mobile => "9090909090"})
+    end
+  end
+  
+  def test_user_is_created_with_given_name_and_mobile
+    user = User.new({:name => "Kamal", :mobile => "9090909090"})
+    assert_equal(user.name, "Kamal")
+    assert_equal(user.mobile, "9090909090")
+    
+  end
+  
+  def test_new_user_default_role_is_general
+    user = User.new({:name => "Kamal", :mobile => "9090909090"})
+    assert_equal(user.role, "general")
+    assert_equal(user.role, Role::GENERAL)
+    assert_not_equal(user.role, Role::STAFF)
+    assert_not_equal(user.role, Role::AFFILIATE)
+    assert_not_equal(user.role, Role::LOYAL)
+  end
+  
+  def test_given_user_is_loyal
+    user = User.new({:name => "Kamal", :mobile => "9090909090", :doj => (Time.now - 3*365*24*60*60)})
+    assert user.loyal?
+  end
+  
+  def test_given_user_is_not_loyal
+    user = User.new({:name => "Kamal", :mobile => "9090909090", :doj => (Time.now - 1.9*365*24*60*60)})
+    assert !user.loyal?
+  end
+  
+end
